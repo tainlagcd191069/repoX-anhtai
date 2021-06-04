d97dc5226de069dc3ee2dec447a3ac8da8008d29
nguyenleanhtai <nguyenleanhtai@localhost>
2021-06-04 07:27:23 +0700

    tai

--- Web/config.php
+++ Web/config.php
@@ -1,13 +1,14 @@
 <?php
-$conn_string = "host=ec2-54-211-176-156.compute-1.amazonaws.com"
-        . " port=5432 dbname=d7k35e83nt54kf"
-        . " user=lkblfujpnveoph"
-        . " password=a2dda41d2a64a9bc1de4ccc447742ff9c32185457810c259326670ce98c0fc42";
+$conn_string = "host=ec2-54-91-188-254.compute-1.amazonaws.com
+"
+        . " port=5432 dbname=d1ei9pf017onc1"
+        . " user=wsuqmbpmsdwjki"
+        . " password=c8d8555244f712d81de30256e9f074c3f0cf12396eaba2ca97691c55546247fa";
 $db = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
    if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+      echo "Opened database successfullytai\n";
    }
 ?>
